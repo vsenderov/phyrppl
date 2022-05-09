9e2026ff5c113e043d5638fc09672c90543752e8
Viktor Senderov <vsenderov@gmail.com>
2022-05-09 16:24:47 +0300

    Newer version of logz plot.

--- tools/logz_plot.R
+++ tools/logz_plot.R
@@ -38,7 +38,7 @@ metadata_from_dir = function(experiment_dir, fields = c("Model",
 plot_logz_directory = function(experiment_dir, xvar = "step", plotf = "plot.svg", title)
 {
   experiment_files = list.files(experiment_dir)
-  experiment_metadata = metadata_from_dir(experiment_dir)
+  experiment_metadata = cbind(metadata_from_dir(experiment_dir), model = as.factor(1:length(experiment_files)))
  
   extract_logz = function(experiment_nr) {
     logz_file = paste0(experiment_dir, "//", experiment_files[experiment_nr], "//logz.txt")
@@ -53,11 +53,72 @@ plot_logz_directory = function(experiment_dir, xvar = "step", plotf = "plot.svg"
   }))
   edata$step = as.numeric(edata$step)
   edata$depth = as.numeric(edata$depth)
+  edata$nsteps = 1/edata$step
   
   ggplot(data = edata, mapping = aes_string(x = xvar, y = "logz")) + 
-    geom_point()  + geom_smooth() + ggtitle(title)
-  
+    geom_point() + geom_smooth(method="gam", formula = y ~ s(x, bs = "cs", k=K)) + 
+    
+    ggtitle(title)
 }
+################################################################################
+plot_logz_directory_box = function(experiment_dir, xvar = "step", plotf = "plot.svg", title)
+{
+  experiment_files = list.files(experiment_dir)
+  experiment_metadata = metadata_from_dir(experiment_dir)
+  
+  extract_logz = function(experiment_nr) {
+    logz_file = paste0(experiment_dir, "//", experiment_files[experiment_nr], "//logz.txt")
+    logz = as.numeric(readLines(logz_file))
+    data.frame(epxeriment_nr = as.factor(rep(experiment_nr, length(logz))), logz = logz)
+  }
+  
+  data = do.call(rbind, lapply(1:length(experiment_files), extract_logz))
+  
+  edata =  do.call(rbind, lapply ( 1:nrow(data) , function(i) {
+    cbind(data[i,], experiment_metadata[data[i,1],])
+  }))
+  edata$step = as.numeric(edata$step)
+  edata$depth = as.factor(as.numeric(edata$depth))
+  edata$nsteps = as.factor(round(1/edata$step))
+  
+  
+  ggplot(data = edata, mapping = aes_string(x = xvar, y = "logz")) + 
+    geom_boxplot() +
+    #geom_violin()+ geom_jitter(alpha = 0.1, width = 0.15) +
+    ggtitle(title)
+}
+################################################################################
+plot_logz_directory_violin = function(experiment_dir, xvar = "step", plotf = "plot.svg", title)
+{
+  experiment_files = list.files(experiment_dir)
+  models = sapply(experiment_files, function(ef) {
+    readLines(file.path(experiment_dir, ef, "model.txt"))
+  })
+  experiment_metadata = cbind(metadata_from_dir(experiment_dir), model = models)
+  
+  extract_logz = function(experiment_nr) {
+    logz_file = paste0(experiment_dir, "//", experiment_files[experiment_nr], "//logz.txt")
+    logz = as.numeric(readLines(logz_file))
+    data.frame(epxeriment_nr = as.factor(rep(experiment_nr, length(logz))), logz = logz)
+  }
+  
+  data = do.call(rbind, lapply(1:length(experiment_files), extract_logz))
+  
+  edata =  do.call(rbind, lapply ( 1:nrow(data) , function(i) {
+    cbind(data[i,], experiment_metadata[data[i,1],])
+  }))
+  edata$step = as.numeric(edata$step)
+  edata$depth = as.factor(as.numeric(edata$depth))
+  edata$nsteps = as.factor(round(1/edata$step))
+  
+  ggplot(data = edata, mapping = aes_string(x = xvar, y = "logz")) + 
+    geom_violin()+ geom_jitter(alpha = 0.1, width = 0.15) +
+    ggtitle(title) + geom_smooth(method="gam", aes(x = as.numeric(depth), y=logz), formula = y ~ s(x, bs = "cs", k=K)) 
+}
+################################################################################
+
+
+
   
 percentage_loss_plot = function(experiment_dir, xvar, title) {
   experiment_files = list.files(experiment_dir)
@@ -70,15 +131,17 @@ percentage_loss_plot = function(experiment_dir, xvar, title) {
   }
   
   data = do.call(rbind, lapply(1:length(experiment_files), extract_logz))
+ 
   loss = do.call(rbind, lapply(unique(data$epxeriment_nr), function(i) {
     ix = data$epxeriment_nr == i
-    cbind("loss" = sum(is.na(data[ix, 2])), experiment_metadata[i,])
+    cbind("loss" = sum(is.na(data[ix, 2]))/length(data[ix, 2])*100, experiment_metadata[i,])
   }))
+    loss$nsteps = 1/as.numeric(loss$step)
   loss[[xvar]] = as.numeric(loss[[xvar]])
-  
+
   ggplot(data=loss, aes_string(x=xvar, y="loss")) +
     geom_line()+
-    geom_point() + ggtitle(title)
+    geom_point() + ggtitle(title) 
 }
   
 logz_violin = function(experiment_dir, plotf = "plot.svg")
@@ -103,16 +166,3 @@ logz_violin = function(experiment_dir, plotf = "plot.svg")
   
 }
 
-
-
-plot_logz_directory("experiments/guards", xvar = "depth", title = "Effect of maximum recursion depth on log Z in AnaDS-GBM for step of 0.125")
-percentage_loss_plot("experiments/guards", xvar = "depth", "% of computations lost with maximum depth setting")
-plot_logz_directory("experiments/rescaling", xvar = "step", title = "Effect of step size (while rescaling the variance) on log Z for AnaDS-GBM")
-percentage_loss_plot("experiments/rescaling", xvar = "step",  "% of computations lost with certain step size")
-#plot_logz_directory("experiments/models", xvar = "epxeriment_nr")
-
-logz_violin("experiments/models/") +  annotate("Text", x = 1, y = -581, label = "AnaDS1", size = 10) +
-  annotate("Text", x = 2, y = -587, label = "AnaDS2", size = 10) +
-  annotate("Text", x = 3, y = -581, label = "AnaDS-GBM", size = 10) + ggtitle("Comparison between models of gradual evolution")
-
-  
